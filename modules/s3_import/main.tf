6f2316ed30f96937b7b7eae47d1f6a338cdd133b
user-1-at-698814524746 <mohd.nasir@firminiq.com>
2025-03-04 17:29:10 +0530

    hello

--- modules/s3_import/main.tf
+++ modules/s3_import/main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "s3_import" {
     bucket_name = var.bucket_name
 
     tags = {
-        Name = dev-voltron-test-project
+        Name = "dev-voltron-test-project"
     }
 
 }
