0daad85db8b5e9a0dcec62d0cd7ffc1a3d05dff4
mdnasirdu957 <108132599+mdnasirdu957@users.noreply.github.com>
2025-03-04 15:13:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,25 +26,25 @@ module "ec2" {
 
 module "s3" {
   source        = "./modules/s3"
-  bucket_name   = "${var.environment}-omron-vsapp-1"
+  bucket_name   = "${var.environment}-omron-vitalsight-1"
   versioning    = true
   force_destroy = false
   logging       = true
-  logging_bucket = "${var.environment}-omron-vsapp-1"
+  logging_bucket = "${var.environment}-omron-vitalsight-1"
 }
 
 
 module "cdn" {
   source               = "./modules/cdn"
-  bucket_name          = "${var.environment}-omron-vsapp-1"
+  bucket_name          = "${var.environment}-omron-vitalsight-1"
   acl                  = "private"
   versioning           = true
   force_destroy        = true
   logging              = true
-  logging_bucket       = "${var.environment}-omron-vsapp-1"
+  logging_bucket       = "${var.environment}-omron-vitalsight-1"
   cdn_enabled          = true
   cdn_price_class      = "PriceClass_100"
-  origin_domain_name   = "${var.environment}-omron-vsapp-1.s3.amazonaws.com"
+  origin_domain_name   = "${var.environment}-omron-vitalsight-1.s3.amazonaws.com"
 }
 
 
