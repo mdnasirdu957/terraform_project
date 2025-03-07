53d3af8f37453a16bf9731d537c2d24f5582d0a9
mdnasirdu957 <108132599+mdnasirdu957@users.noreply.github.com>
2025-03-07 15:51:21 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "main" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   subnet_id       = var.subnet_id
-  security_groups = [var.security_group]
+  vpc_security_group_ids = [var.security_group]
   associate_public_ip_address = "true"
   tags = {
     Name = "${var.environment}-omron-1"
