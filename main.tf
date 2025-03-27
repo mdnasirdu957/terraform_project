30d55aa2a72124bc75db4fddaecb9c6b317a8dcb
mdnasirdu957 <108132599+mdnasirdu957@users.noreply.github.com>
2025-03-27 23:35:13 +0530

    removed code

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-module "vpc" {
-  source     = "./modules/vpc"
-  vpc_cidr   = var.vpc_cidr
-}
-
-module "subnet" {
-  source     = "./modules/subnet"
-  vpc_id     = module.vpc.vpc_id
-  subnet_cidr = var.subnet_cidr
-  az = "ap-south-1a"
-}
-
-module "security_group" {
-  source      = "./modules/security-group"
-  vpc_id      = module.vpc.vpc_id
-}
-
-module "ec2" {
-  source         = "./modules/ec2"
-  subnet_id      = module.subnet.subnet_id
-  security_group = module.security_group.sg_id
-  instance_type  = var.instance_type
-  ami_id         = var.ami_id
-  environment = "${var.environment}"
-}
 
 module "s3" {
   source        = "./modules/s3"
