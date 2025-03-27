02be59bf8cf5908ecab428c3170d294844d7c4c8
mdnasirdu957 <108132599+mdnasirdu957@users.noreply.github.com>
2025-03-28 02:01:30 +0530

    new infra updated

--- main.tf
+++ main.tf
@@ -6,6 +6,37 @@ module "s3" {
   force_destroy = false
 }
 
+resource "aws_s3_bucket" "New_bucket" {
+ bucket_name = "dev-omron-vital-2"
+
+}
+
+module "vpc" {
+  source     = "./modules/vpc"
+  vpc_cidr   = var.vpc_cidr
+}
+
+module "subnet" {
+  source     = "./modules/subnet"
+  vpc_id     = module.vpc.vpc_id
+  subnet_cidr = var.subnet_cidr
+  az = "ap-south-1a"
+}
+
+module "security_group" {
+  source      = "./modules/security-group"
+  vpc_id      = module.vpc.vpc_id
+}
+
+module "ec2" {
+  source         = "./modules/ec2"
+  subnet_id      = module.subnet.subnet_id
+  security_group = module.security_group.sg_id
+  instance_type  = var.instance_type
+  ami_id         = var.ami_id
+  environment = "${var.environment}"
+}
+
 
 # module "cdn" {
 #   source               = "./modules/cdn"
