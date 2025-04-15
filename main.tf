a731ec6581cbc5dc7ee26ee87d29c88b6d521cb1
mdnasirdu957 <108132599+mdnasirdu957@users.noreply.github.com>
2025-04-15 17:27:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 module "s3" {
   source        = "./modules/s3"
   bucket_name   = "${var.environment}-omron-vitalsight-1"
-  Versioning = "Disabled"
+  versioning    = "Disabled"
 }
 
 resource "aws_s3_bucket" "New_bucket" {
